12dfa5176a8ce83ec01ba9688452fa86910c8947
whotterre <iJeddy5.0@gmail.com>
2025-07-15 00:36:14 +0100

    Implement RedisTaskDistributor for task distribution using asynq - a skeleton for the bg process stuff

---
+++ worker/distributor.go
@@ -0,0 +1,23 @@
+package worker
+
+import "github.com/hibiken/asynq"
+
+type TaskDistributor interface {
+
+}
+
+type RedisTaskDistributor struct {
+	client *asynq.Client
+}
+
+func NewRedisTaskDistributor(redisOpt asynq.RedisClientOpt) TaskDistributor {
+	client := asynq.NewClient(redisOpt)
+	return &RedisTaskDistributor{
+		client: client,
+	}
+}
+
+
+
+
+
